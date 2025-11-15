c91991c3f9909189474ae706d918a65a43cae07b
Robert Pratt <rpratt1337@gmail.com>
2025-11-15 06:45:55 -0600

    refactor(types): update index exports for unified type organization

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,34 @@
-export * as colors from './colors'
-export * as core from './core'
-export * as form from './form'
-export * as moxfield from './moxfield'
-export * as supabase from './supabase'
+export * from './core'
+export * from './colors'
+export * from './form'
+export * from './analysis'
+
+export {
+  type MoxfieldDeck,
+  type CreatedByUser,
+  type CardEntry,
+  type Boards,
+  type Affiliates,
+  type AuthorTags,
+  type Hub,
+  type Media,
+  Color,
+  type ColorPercentages,
+  type Legalities,
+  type Prices,
+  type Main,
+  Format,
+  type OriginalDeck,
+  type Token,
+  type CardMappings,
+  type DeckStats,
+  type MoxfieldSyncParams,
+  type MoxfieldSyncResponse,
+  type SyncStatus,
+  type RawMoxData,
+  type Commander,
+  type Card as MoxfieldCard,
+  type CardFace as MoxfieldCardFace,
+} from './moxfield'
+
+export type { Database } from './supabase/generated'
